745b6b0ec9697f11b7498fd28f179550e32dafc8
Nikita Kotenkov <80511554+PrinceNanadaime@users.noreply.github.com>
2021-12-21 19:47:03 +0300

    Update BrokerImpl.java

--- src/main/java/mqttcamera/broker/BrokerImpl.java
+++ src/main/java/mqttcamera/broker/BrokerImpl.java
@@ -57,6 +57,7 @@ public class BrokerImpl implements Broker, MqttCallback {
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
         if (!Objects.equals(topic, "devices/camera")) throw new IllegalArgumentException();
+        System.out.println("message is : "+ Arrays.toString(message.getPayload()));
         if (Arrays.toString(message.getPayload()).equals("TRUE")){
             alarmEntityLongMqttRepository.save(new AlarmEntity(
                     cameraEntityLongMqttRepository.findById(1L).get(),new Date(1L),"Motion detected!"));
